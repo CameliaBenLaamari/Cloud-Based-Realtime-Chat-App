2a101ea7957f1d99eb9b950c3865fcc2b5c04989
Camelia Ben Laamari <camelia.benlaamari@supcom.tn>
2021-10-12 18:54:26 +0100

    Adding Heroku Deployment Files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles.css'
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
